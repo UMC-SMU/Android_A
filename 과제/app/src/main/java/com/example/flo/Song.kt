47370bd8c7bd0f2b9fa09f0496bfb6ab1a9827e9
kimby <kimby2005@naver.com>
2022-04-08 21:20:08 +0900

    4주차 과제
    안드로이드 4주차 과제: 스레드 관련

--- 과제/app/src/main/java/com/example/flo/Song.kt
+++ 과제/app/src/main/java/com/example/flo/Song.kt
@@ -2,5 +2,8 @@ package com.example.flo
 
 data class Song(
     val title : String = "",
-    val singer : String = ""
+    val singer : String = "",
+    var second: Int = 0, // 재생 시간
+    var playTime: Int = 0, // 총 재생 시간
+    var isPlaying: Boolean = false
 )
